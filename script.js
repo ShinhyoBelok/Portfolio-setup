0670abcea818dd0461f121da7135f00d31aa9d70
shinhyo26okazaki <shibo6937@gmail.com>
2022-11-14 17:31:26 -0400

    Add fixed navbar and fix linters errors

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const openMenu = document.querySelector(".open-menu");
-const menu = document.querySelector(".menu");
-const closeMenu = document.querySelector(".close-menu");
-const navItems = document.querySelectorAll(".nav-item");
+const openMenu = document.querySelector('.open-menu');
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu');
+const navItems = document.querySelectorAll('.nav-item');
 
-openMenu.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  openMenu.classList.toggle("hide");
-  closeMenu.classList.toggle("show");
+openMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  openMenu.classList.toggle('hide');
+  closeMenu.classList.toggle('show');
 });
 
-closeMenu.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  openMenu.classList.toggle("show");
-  openMenu.classList.toggle("hide");
-  closeMenu.classList.toggle("show");
+closeMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  openMenu.classList.toggle('show');
+  openMenu.classList.toggle('hide');
+  closeMenu.classList.toggle('show');
 });
 
 navItems.forEach((navItem) => {
-  navItem.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    openMenu.classList.toggle("show");
-    openMenu.classList.toggle("hide");
-    closeMenu.classList.toggle("show");
+  navItem.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    openMenu.classList.toggle('show');
+    openMenu.classList.toggle('hide');
+    closeMenu.classList.toggle('show');
   });
 });
